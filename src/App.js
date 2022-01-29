584bff6b70a6ddee5686c19fdf0a408f934e73de
Usmanov Marat Ravshanovich <checlioschew@yandex.ru>
2022-01-29 15:29:57 +0300

    base components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from 'react';
-import { DatePicker } from 'antd';
-import logo from './logo.svg';
+import {DatePicker} from 'antd';
 import 'antd/dist/antd.min.css';
 import './App.css';
 import MyButton from "./components/MyButton";
 import HeaderMenu from "./components/HeaderMenu/HeaderMenu";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <DatePicker/>
-          <MyButton value="pidaraz" />
-          <HeaderMenu />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <DatePicker/>
+                <HeaderMenu/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './HeaderMenu.module.scss';
+import MyButton from "../MyButton";
 
 export default function HeaderMenu(){
     return <div>
-        <h1 className={styles.error}>Menu</h1>
-        <h2 className={styles.ok}>points</h2>
+        <MyButton value="ОБОРУДОВАНИЕ"/>
+        <MyButton value="ИНСТРУМЕНТЫ"/>
+        <MyButton value="РАСХОДКА" />
+        <MyButton value="ОТЧЕТЫ" />
+        <MyButton value="БАЗА" />
     </div>
 }
